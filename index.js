a5418a4afbaa756cf58ad3c7d5b53a2d751a11a0
Ezequiel-H <horowitzeze@gmail.com>
2024-03-20 13:52:28 -0300

    feat: adding transaction error

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const port = 3000
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.post('/', (req, res) => {
-  console.log(req.body)
-  console.log(req.body.email)
-  res.send(req.body)
-})
+  console.log(req.body);
+  console.log(req.body.email);
+  res.send({ ...req.body, transaction: 'error' });
+});
 
 app.get('/health', (req, res) => {
-  res.send('OK')
-})
+  res.send('OK');
+});
 
 app.get('/healthz', (req, res) => {
-  res.send('OK')
-})
+  res.send('OK');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
